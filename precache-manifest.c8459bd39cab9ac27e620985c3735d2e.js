6a7f01806383c10873ec1a969734f23f7c2d2ca6
frankie <frankiechau20044e@yahoo.com.hk>
2020-06-08 22:07:09 +0800

    Updates

---
+++ precache-manifest.c8459bd39cab9ac27e620985c3735d2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ec17330dfd976e139b9772dd8c242e",
+    "url": "/react-pdf-editor/index.html"
+  },
+  {
+    "revision": "49a92d8940fd06524a66",
+    "url": "/react-pdf-editor/static/css/main.03e28f0d.chunk.css"
+  },
+  {
+    "revision": "98b27260e96db8e772f8",
+    "url": "/react-pdf-editor/static/js/2.d0be57a1.chunk.js"
+  },
+  {
+    "revision": "ef1c1dc921eb892faf80c713a1f15928",
+    "url": "/react-pdf-editor/static/js/2.d0be57a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a92d8940fd06524a66",
+    "url": "/react-pdf-editor/static/js/main.2ba1aaf1.chunk.js"
+  },
+  {
+    "revision": "667c89dfb35b293c8cab",
+    "url": "/react-pdf-editor/static/js/runtime-main.cd0b203a.js"
+  }
+]);
