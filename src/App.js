38c1307b704f6cd73dc0ee79027e6ebc98faebc3
frankie <frankiechau20044e@yahoo.com.hk>
2020-06-08 22:18:15 +0800

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react';
 import './App.css';
+import * as jsPDF from 'jspdf'
 
+var doc
+const generatePDF = (text) => {
+  doc = new jsPDF({format: 'a4'});
+  try {
+    eval(text)
+  } catch (e) {}
+  return doc.output('datauristring');
+}
 function App() {
+
+  const [string,setString] = useState(generatePDF(`doc.text("Hello world!", 20, 20);
+  doc.text("This is client-side Javascript, pumping out a PDF.", 20, 30);`))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="split left">
+        <Input setString={setString} />
+      </div>
+      <div class="split right">
+        <embed width='100%' height='100%' src={string} />
+      </div>
     </div>
   );
 }
+const Input = ({setString}) => {
+  
+  const [text, setText] = useState(`doc.text("Hello world!", 20, 20);
+  doc.text("This is client-side Javascript, pumping out a PDF.", 20, 30);`
+)
 
+  return <><textarea id="w3review" name="w3review" rows="4" cols="50" onChange={e => setText(e.target.value)}>
+    {text}
+  </textarea>
+  <br/>
+  <button onClick={()=>setString(generatePDF(text))} type="primary">update</button>
+  <br/>
+  <button onClick={()=>doc.save("output")} type="primary">download</button>
+  </>
+
+}
 export default App;
